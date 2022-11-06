e60f07be4166f23125b8bc850386b8e3a86ef9fa
Saksham Thapa <56692835+Saksham010@users.noreply.github.com>
2022-11-06 15:43:30 +0545

    Update NFTpage.js

--- src/components/marketplace/NFTpage.js
+++ src/components/marketplace/NFTpage.js
@@ -79,31 +79,35 @@ async function buyNFT(tokenId) {
         <div>
             <Navbar/>
             <div className="nftpageContainer">
-                <img src={data.image} alt="" className="w-2/5" />
-                <div className="text-xl ml-20 space-y-8 text-white shadow-2xl rounded-lg border-2 p-5">
-                    <div>
-                        Name: {data.name}
+                <div className="externalContainer">
+
+                    <img src={data.image} alt="" className="w-2/5" />
+                    <div className="text-xl ml-20 space-y-8 text-white shadow-2xl rounded-lg border-2 p-5">
+                        <div>
+                            Name: {data.name}
+                        </div>
+                        <div>
+                            Description: {data.description}
+                        </div>
+                        <div>
+                            Price: <span className="">{data.price + " ETH"}</span>
+                        </div>
+                        <div>
+                            Owner: <span className="text-sm">{data.owner}</span>
+                        </div>
+                        <div>
+                            Seller: <span className="text-sm">{data.seller}</span>
+                        </div>
+                        <div>
+                        { currAddress == data.owner?<div className="text-emerald-700">You are the owner of this NFT</div> :
+                            <button className="enableEthereumButton bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded text-sm" onClick={() => buyNFT(tokenId)}>Buy this NFT</button>
+                        
+                        }
+                        
+                        <div className="text-green text-center mt-3">{message}</div>
+                        </div>
                     </div>
-                    <div>
-                        Description: {data.description}
-                    </div>
-                    <div>
-                        Price: <span className="">{data.price + " ETH"}</span>
-                    </div>
-                    <div>
-                        Owner: <span className="text-sm">{data.owner}</span>
-                    </div>
-                    <div>
-                        Seller: <span className="text-sm">{data.seller}</span>
-                    </div>
-                    <div>
-                    { currAddress == data.owner?<div className="text-emerald-700">You are the owner of this NFT</div> :
-                        <button className="enableEthereumButton bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded text-sm" onClick={() => buyNFT(tokenId)}>Buy this NFT</button>
-                       
-                    }
-                    
-                    <div className="text-green text-center mt-3">{message}</div>
-                    </div>
+
                 </div>
             </div>
         </div>
